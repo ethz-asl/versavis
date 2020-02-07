d9c1429ebe08efee328bf7bcbda0f4805fbbbbd0
Marius Fehr <marius.b.fehr@gmail.com>
2020-02-07 12:18:26 -0800

    forward synchronized camera info

--- versavis/include/versavis/versavis_synchronizer.h
+++ versavis/include/versavis/versavis_synchronizer.h
@@ -23,6 +23,7 @@
 #include <ros/console.h>
 #include <ros/ros.h>
 #include <ros/subscriber.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/image_encodings.h>
@@ -41,6 +42,7 @@ public:
   ~VersaVISSynchronizer();
 
   void imageCallback(const image_numbered_msgs::ImageNumbered &image_msg);
+  void cameraInfoCallback(const sensor_msgs::CameraInfo& camera_info_msg);
   void imageTimeCallback(const versavis::TimeNumbered &image_time_msg);
   void publishImg(const image_numbered_msgs::ImageNumbered &image_msg);
 
@@ -56,15 +58,23 @@ private:
   ros::NodeHandle nh_private_;
   image_transport::ImageTransport image_transport_;
   ros::Subscriber image_sub_;
+  ros::Subscriber camera_info_sub_;
   image_transport::Publisher image_fast_pub_;
   image_transport::Publisher image_slow_pub_;
+  // If camera info forwarding is enabled, then the CameraPublishers are used.
+  image_transport::CameraPublisher fast_camera_pub_;
+  image_transport::CameraPublisher slow_camera_pub_;
   ros::Publisher initialized_pub_;
   ros::Subscriber image_time_sub_;
 
   // Topic names.
+  std::string versavis_topic_;
   std::string driver_topic_;
+  std::string camera_info_sub_topic_;
   std::string image_fast_pub_topic_;
+  std::string camera_info_fast_pub_topic_;
   std::string image_slow_pub_topic_;
+  std::string camera_info_slow_pub_topic_;
   std::string image_time_sub_topic_;
   std::string initialized_pub_topic_;
 
@@ -72,6 +82,8 @@ private:
   versavis::TimeNumbered init_time_;
   std::vector<versavis::TimeNumbered> image_time_stamp_candidates_;
   std::vector<image_numbered_msgs::ImageNumbered> image_candidates_;
+  bool received_first_camera_info_;
+  sensor_msgs::CameraInfo camera_info_msg_;
   ros::Time last_stamp_;
   ros::Time init_timestamp_;
 
@@ -87,9 +99,10 @@ private:
   int64_t offset_;
   bool initialized_;
 
-  // Conifugration.
+  // Configuration.
   uint8_t slow_publisher_image_counter_;
   bool publish_slow_images_;
+  bool forward_camera_info_;
   ros::Duration imu_offset_;
 
   std::mutex mutex_;
