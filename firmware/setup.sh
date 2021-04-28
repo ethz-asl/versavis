b811be1033afa7b02c840d045076b8e2d4c23ead
Florian Tschopp <ftschopp@ethz.ch>
2021-04-28 14:14:09 +0200

    change method to detect current shell (#30)
    * change method to detect current shell
    
    * switch to hard code and shebang

--- firmware/setup.sh
+++ firmware/setup.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
 catkin build versavis
-shell=`grep ^$(id -un): /etc/passwd | cut -d : -f 7- |  cut -d'/' -f4-`
 devel=/`catkin config | grep ^"Devel Space:" |  cut -d'/' -f2-`
-source_path="$devel"/setup."$shell"
+source_path="$devel"/setup.bash
 
 echo $source_path
 source $source_path
